7aac1d68e1dea0861689f4da4269e8e36efac95e
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-04-17 13:30:33 -0700

    Update sramk002_lab4_part2.c

--- turnin/sramk002_lab4_part2.c
+++ turnin/sramk002_lab4_part2.c
@@ -1,13 +1,12 @@
-        
-/*      Author: Sidharth Ramkumar (sramk002@ucr.edu)
- *  Partner(s) Name: 
- *      Lab Section: 022
- *      Assignment: Lab #4  Exercise #2
- *      Exercise Description: [optional - include for your own benefit]
- *
- *      I acknowledge all content contained herein, excluding template or example
- *      code, is my own original work.
- */
+/*      Author Sidharth Ramkumar (sramk002@ucr.edu) 
+ *       *  Partner(s) Name: 
+ *        *      Lab Section: 022
+ *         *      Assignment: Lab #4  Exercise #2
+ *          *      Exercise Description: [optional - include for your own benefit]
+ *           *
+ *            *      I acknowledge all content contained herein, excluding template or example
+ *             *      code, is my own original work.
+ *              */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
@@ -16,7 +15,7 @@
 #define b1 (PINA & 0x01)
 #define b2 (PINA & 0x02)
 
-enum Num_States { SMStart, Wait, Inc, Dec, Reset} Num_State;
+enum Num_States { SMStart, Wait, Inc, Inc_Wait, Dec, Dec_Wait, Reset } Num_State;
 
 void TickFct(short *tmpC){
    switch(Num_State) {
@@ -32,27 +31,43 @@ void TickFct(short *tmpC){
         } else if (b1 && b2){
         Num_State = Reset;
         } else {
-        Num_State = Wait;        
+        Num_State = Wait;
         }
         break;
 
         case Inc:
-        if (b1 && !b2){
-        Num_State = Inc;
-        } else if (!b1 && !b2){
-        Num_State = Wait;
-        } else if (b1 && b2){
+        if (b1 && b2){
         Num_State = Reset;
+        } else {
+        Num_State = Inc_Wait;
         }
         break;
 
-        case Dec:
-        if (!b1 && b2){
-        Num_State = Dec;
-        } else if (!b1 && !b2){
-        Num_State = Wait;
+        case Inc_Wait:
+        if (b1 && !b2){
+        Num_State = Inc_Wait;
         } else if (b1 && b2){
         Num_State = Reset;
+        } else if (!b1 && !b2) {
+        Num_State = Wait;
+        }
+        break;
+        
+        case Dec:
+        if (b1 && b2){
+        Num_State = Reset;
+        } else {
+        Num_State = Dec_Wait;
+        }
+        break;
+                   
+        case Dec_Wait:
+        if (!b1 && b2){
+        Num_State = Dec_Wait;
+        } else if (b1 && b2) {
+        Num_State = Reset;
+        } else {
+        Num_State = Wait;
         }
         break;
 
@@ -67,11 +82,15 @@ void TickFct(short *tmpC){
 
    switch (Num_State){
         case Inc:
+        if (*tmpC < 9){
         *tmpC += 1;
+        }
         break;
 
         case Dec:
+        if (*tmpC > 0){
         *tmpC -= 1;
+        }
         break;
 
         case Reset:
@@ -95,3 +114,4 @@ int main(void) {
     }
     return 1;
 }
+    
